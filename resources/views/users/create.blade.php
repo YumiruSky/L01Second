31f09d294a0ea0cd8433e41442c0b963edd180e3
YumiruSky <YumiruSky@gmail.com>
2018-11-15 06:38:33 +0000

    Finish layout and routes

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','signup')
+@section('content')
+<h1>Signup</h1>
+@stop
+
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,13 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//StaticPagesController
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/about','StaticPagesController@about')->name('about');
+
+//UsersController
+Route::get('/signup','UsersController@create')->name('signup');
+
