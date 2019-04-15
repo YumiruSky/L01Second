25c79cd99a96a459f7fc052169b30e0931953042
YumiruSky <YumiruSky@gmail.com>
2019-04-15 05:04:08 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
-
+use Auth;
 
 class UsersController extends Controller
 {
@@ -32,7 +32,8 @@ class UsersController extends Controller
 		  'email'=> $request->email,
 		  'password'=> bcrypt($request->password)
 		]);
-		
+
+		Auth::login($user);
 		session()->flash('success','注册成功');
 		return redirect()->route('users.show',[$user]);
 	}
